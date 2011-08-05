1192c298f81b0683a04fc6d8d2b1cb34f4183175
Anton Barty <anton.barty@desy.de>
2011-08-04 17:50:09 -0700

    Cheetah now appears to be config_v3 compliant

--- cheetah.cpp
+++ cheetah.cpp
@@ -16,6 +16,7 @@
 #include "release/pdsdata/cspad/ConfigV1.hh"
 #include "release/pdsdata/cspad/ConfigV2.hh"
 #include "release/pdsdata/cspad/ConfigV3.hh"
+
 #include "release/pdsdata/cspad/ElementHeader.hh"
 #include "release/pdsdata/cspad/ElementIterator.hh"
 #include "cspad-gjw/CspadTemp.hh"
