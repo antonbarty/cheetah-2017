358312e32a80191e76a5b501fcfb938cfac8870c
Filipe Maia <filipe.c.maia@gmail.com>
2013-03-28 18:50:45 -0700

    Make it compile cleanly on psexport

--- cmake/Modules/FindPSANA.cmake
+++ cmake/Modules/FindPSANA.cmake
@@ -1,17 +1,16 @@
 
-find_file(ANA_CURRENT_ROOT ana-current /opt/psana/g/psdm/portable/sw/releases/)
+find_file(ANA_CURRENT_ROOT ana-current /opt/psana/g/psdm/portable/sw/releases/ /reg/g/psdm/sw/releases/ /cfel/common/slac/reg/g/psdm/portable/sw/releases/)
 
 file(READ /etc/redhat-release redhat_release)
 string(REGEX MATCH " 6\\." rhel6 ${redhat_release})
 string(REGEX MATCH " 5\\." rhel5 ${redhat_release})
 if(rhel6)
- set(arch "rhel6")
+  set(ANA_ARCH "x86_64-rhel6-gcc44-opt" CACHE STRING "ana architecture to be used")
 endif(rhel6)
 if(rhel5)
- set(arch "rhel5")
+  set(ANA_ARCH "x86_64-rhel5-gcc41-opt" CACHE STRING "ana architecture to be used")
 endif(rhel5)
 
-set(ANA_ARCH "x86_64-${arch}-gcc44-opt" CACHE STRING "ana architecture to be used")
 
 LIST(APPEND ana_libs ErrSvc PSTime rt PSEvt AppUtils acqdata andordata bld camdata compressdata controldata
   cspad2x2data cspaddata encoderdata epics evrdata fccddata fexampdata flidata gsc16aidata indexdata
