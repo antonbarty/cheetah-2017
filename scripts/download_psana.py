93119c6b606a60047f6392fc35a4248ff562ca31
Filipe Maia <filipe.maia@icm.uu.se>
2013-04-04 12:10:44 +0200

    Fix missing files

--- scripts/download_psana.py
+++ scripts/download_psana.py
@@ -15,3 +15,5 @@ ana_version = sys.argv[1]
 output_dir = sys.argv[2]
 
 os.system('ssh psexport.slac.stanford.edu "cd /reg/g/psdm/sw/releases/ && tar czf - '+ana_version+'" | tar xzvf - -C '+output_dir)
+os.system('ssh psexport.slac.stanford.edu "cd /reg/g/psdm/sw/external/pdsdata/6.1.4/x86_64-rhel6-gcc44-opt && tar czf - pdsdata" | tar xzvf - -C '+output_dir+'/'+ana_version+'/include)
+os.system('ssh psexport.slac.stanford.edu "cd /reg/g/psdm/sw/external/root/5.30.06-python2.7/x86_64-rhel6-gcc44-opt/include && tar czf - root" | tar xzvf - -C '+output_dir+'/'+ana_version+'/include')
