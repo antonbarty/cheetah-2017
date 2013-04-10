f22b2c68f6904e102663d73eb49f311b8b38de66
hantke@xray.bmc.uu.se <max.hantke@stud.uni-goettingen.de>
2013-04-10 09:54:46 +0200

    saveCXI_mutex added

--- libcheetah/src/worker.cpp
+++ libcheetah/src/worker.cpp
@@ -300,9 +300,9 @@ void *worker(void *threadarg) {
  save:
   if((hit && global->savehits) || ((global->hdf5dump > 0) && ((eventData->frameNumber % global->hdf5dump) == 0))){
     if(global->saveCXI==1){
-      pthread_mutex_lock(&global->selfdark_mutex);
+      pthread_mutex_lock(&global->saveCXI_mutex);
       writeCXI(eventData, global);
-      pthread_mutex_unlock(&global->selfdark_mutex);
+      pthread_mutex_unlock(&global->saveCXI_mutex);
     }
     else {
       writeHDF5(eventData, global);
