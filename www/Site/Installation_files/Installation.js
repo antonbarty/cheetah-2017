1a388d47f9a3893e65d34c19ea4e38c3f934783b
Anton Barty <anton.barty@desy.de>
2013-04-17 10:08:57 +0200

    Started new web pages

---
+++ www/Site/Installation_files/Installation.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130417
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Installation_files/InstallationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
