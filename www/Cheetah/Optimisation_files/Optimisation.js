c0fb6b8043cdcae487794582e48d8df8e0474941
Anton Barty <anton.barty@desy.de>
2013-04-18 14:46:15 +0200

    Changes to the web site

---
+++ www/Cheetah/Optimisation_files/Optimisation.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130418
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Optimisation_files/OptimisationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
