ee5954cc2a1b21a3362bd1584c1e7a960f9f7072
Anton Barty <anton.barty@desy.de>
2013-11-01 15:57:37 -0700

    Added BlsDataEBeamV4 (needed to read wavelength etc as of 1 November)

--- source/cheetah_ana_pkg/src/cheetah_ana_mod.cpp
+++ source/cheetah_ana_pkg/src/cheetah_ana_mod.cpp
@@ -366,11 +366,35 @@ namespace cheetah_ana_pkg {
         double peakCurrent = 0;
 		double DL2energyGeV = 0;
 	
+		shared_ptr<Psana::Bld::BldDataEBeamV4> ebeam4 = evt.get(m_srcBeam);
 		shared_ptr<Psana::Bld::BldDataEBeamV3> ebeam3 = evt.get(m_srcBeam);
 		shared_ptr<Psana::Bld::BldDataEBeamV2> ebeam2 = evt.get(m_srcBeam);
 		shared_ptr<Psana::Bld::BldDataEBeamV1> ebeam1 = evt.get(m_srcBeam);
 		shared_ptr<Psana::Bld::BldDataEBeamV0> ebeam0 = evt.get(m_srcBeam);
 
+        // Ebeam v4
+		if (ebeam4.get()) {
+			charge = ebeam4->ebeamCharge();
+			L3Energy = ebeam4->ebeamL3Energy();
+			LTUPosX = ebeam4->ebeamLTUPosX();
+			LTUPosY = ebeam4->ebeamLTUPosY();
+			LTUAngX = ebeam4->ebeamLTUAngX();
+			LTUAngY = ebeam4->ebeamLTUAngY();
+			PkCurrBC2 = ebeam4->ebeamPkCurrBC2();
+			
+            peakCurrent = ebeam4->ebeamPkCurrBC2();
+			DL2energyGeV = 0.001*ebeam4->ebeamL3Energy();
+			
+			if (verbose) {
+				cout << "* fEbeamCharge4=" << charge << "\n"
+                << "* fEbeamL3Energy4=" << L3Energy << "\n"
+                << "* fEbeamLTUPosX4=" << LTUPosX << "\n"
+                << "* fEbeamLTUPosY4=" << LTUPosY << "\n"
+                << "* fEbeamLTUAngX4=" << LTUAngX << "\n"
+                << "* fEbeamLTUAngY4=" << LTUAngY << "\n"
+                << "* fEbeamPkCurrBC24=" << PkCurrBC2 << endl;
+			}
+		}
         // Ebeam v3
 		if (ebeam3.get()) {
 			charge = ebeam3->ebeamCharge();
