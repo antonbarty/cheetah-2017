ec1122bdf8b77e52bcdd6e2fb8793244d1d0429c
Anton Barty <anton.barty@desy.de>
2013-12-03 15:31:32 +0100

    Updates to the online documentation

--- www/Cheetah/Cheetah_at_home_files/Cheetah_at_home.js
+++ www/Cheetah/Cheetah_at_home_files/Cheetah_at_home.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('FAQ_files/FAQMoz.css')
+{loadMozillaCSS('Cheetah_at_home_files/Cheetah_at_homeMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
