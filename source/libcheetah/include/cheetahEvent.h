e10914d5a8c8b1a6f6fa590372f02fc6850441d0
hantke@xray.bmc.uu.se <hantke@xray.bmc.uu.se>
2014-01-07 18:16:36 +0100

    merged with developer, compiled and tested on one dataset from July 2012 at AMO.

--- source/libcheetah/include/cheetahEvent.h
+++ source/libcheetah/include/cheetahEvent.h
@@ -134,17 +134,16 @@ public:
 	
 } ;
 
-
 #define ERROR(...) cheetahError(__FILE__, __LINE__, __VA_ARGS__)
 
 void static cheetahError(const char *filename, int line, const char *format, ...){
-	va_list ap;
-	va_start(ap,format);
-	fprintf(stderr,"CHEETAH-ERROR in %s:%d: ",filename,line);
-	vfprintf(stderr,format,ap);
-	va_end(ap);
-	puts("");
-	abort();
+  va_list ap;
+  va_start(ap,format);
+  fprintf(stderr,"CHEETAH-ERROR in %s:%d: ",filename,line);
+  vfprintf(stderr,format,ap);
+  va_end(ap);
+  puts("");
+  abort();
 }
 
 #define DEBUGL1_ONLY if(global->debugLevel >= 1)
