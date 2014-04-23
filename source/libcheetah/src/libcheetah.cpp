2b097d6b72711cf4d2a8f2e20f01794212a74f19
Filipe Maia <filipe.maia@icm.uu.se>
2014-04-23 18:02:15 +0200

    Make sure to only free allocated TOF structures

--- source/libcheetah/src/libcheetah.cpp
+++ source/libcheetah/src/libcheetah.cpp
@@ -265,14 +265,24 @@ void cheetahDestroyEvent(cEventData *eventData) {
 	}
 	//TOF stuff.
 	// Explictly call the vector destructor as we're using free and not delete
-	for(unsigned int i = 0;i<global->TOFChannelsPerCard.size();i++){
-		free(eventData->TOFAllTime[i]);
-		free(eventData->TOFAllVoltage[i]);
-		free(eventData->TOFAllTrigTime[i]);
+	if(eventData->TOFAllTime){
+		for(unsigned int i = 0;i<global->TOFChannelsPerCard.size();i++){
+			free(eventData->TOFAllTime[i]);
+		}
+		free(eventData->TOFAllTime);
 	}
-	free(eventData->TOFAllTime);
-	free(eventData->TOFAllVoltage);
-	free(eventData->TOFAllTrigTime);
+	if(eventData->TOFAllVoltage){
+		for(unsigned int i = 0;i<global->TOFChannelsPerCard.size();i++){
+			free(eventData->TOFAllVoltage[i]);
+		}
+		free(eventData->TOFAllVoltage);	
+	}
+	if(eventData->TOFAllTrigTime){
+		for(unsigned int i = 0;i<global->TOFChannelsPerCard.size();i++){
+			free(eventData->TOFAllTrigTime[i]);
+		}
+		free(eventData->TOFAllTrigTime);
+	}
 
 
 	if(eventData->FEEspec_present == 1) {
