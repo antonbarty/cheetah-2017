b8955c79ff8ff29bf5b037b28ad7575aac9405bc
Filipe Maia <filipe.maia@icm.uu.se>
2014-05-07 22:10:16 +0200

    Ready to merge

--- source/libcheetah/include/saveCXI.h
+++ source/libcheetah/include/saveCXI.h
@@ -10,11 +10,11 @@
 #include <vector>
 
 
-#include "detectorObject.h"
-#include "cheetahGlobal.h"
-#include "cheetahEvent.h"
-#include "cheetahmodules.h"
-#include "median.h"
+#include <detectorObject.h>
+#include <cheetahGlobal.h>
+#include <cheetahEvent.h>
+#include <cheetahmodules.h>
+#include <median.h>
 
 
 namespace CXI{
@@ -44,9 +44,8 @@ namespace CXI{
 
 			id = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id);
 			if( id<0 ) {ERROR("Cannot create file.\n");}
-			stackSlice = 0;
+			stackCounter = 0;
 		}
-		
 		Node(std::string s, hid_t oid, Node * p, Type t){
 			name = s;
 			parent = p;
@@ -92,7 +91,8 @@ namespace CXI{
 		void openAll();
 		std::string path();
 		Node & child(std::string prefix, int n);
-		void trimAll(int stackSize);
+		void trimAll(int stackSize = -1);
+		uint getStackSlice();
 
 		std::string name;
 	private:
@@ -100,172 +100,19 @@ namespace CXI{
 		void addStackAttributes(hid_t dataset, int ndims);
 		hid_t writeNumEvents(hid_t dataset, int stackSlice);
 		std::string nextKey(const char * s);
+		template <class T>
+			hid_t get_datatype(const T * foo);
+
 		typedef std::map<std::string, Node *>::iterator Iter;
 		std::map<std::string,Node *> children;
 		Node * parent;
 		hid_t id;
-		int stackSlice;
 		Type type;
-	};
-
-	typedef struct{
-	  hid_t self;
-	  hid_t translation;
-	}Geometry;
-
-	typedef struct{
-	  hid_t self;
-	  Geometry geometry;
-	}Sample;
-
-	typedef struct{
-		hid_t self;
-		hid_t distance;
-		hid_t data;
-		hid_t tofTime;
-		hid_t mask;
-		hid_t mask_shared;
-		hid_t thumbnail;
-		hid_t description;
-		hid_t xPixelSize;
-		hid_t yPixelSize;
-	}Detector;
-
-
-	typedef struct{
-		/* ID of the group itself */
-		hid_t self;
-		hid_t data;
-		hid_t mask;
-		hid_t mask_shared;
-		hid_t mask_shared_min;
-		hid_t mask_shared_max;
-		hid_t thumbnail;
-		hid_t dataType;
-		hid_t dataSpace;
-	}Image;
-
-	typedef struct{
-		hid_t self;
-
-		hid_t energy;
-	}Source;
-
-	typedef struct{
-		hid_t self;
-
-		Source source;
-		std::vector<Detector> detectors;
-	}Instrument;
-
-	typedef struct{
-		hid_t self;
-
-		hid_t machineTime;
-		hid_t fiducial;
-		hid_t ebeamCharge;
-		hid_t ebeamL3Energy;
-		hid_t ebeamPkCurrBC2;
-		hid_t ebeamLTUPosX;
-		hid_t ebeamLTUPosY;
-		hid_t ebeamLTUAngX;
-		hid_t ebeamLTUAngY;
-		hid_t phaseCavityTime1;
-		hid_t phaseCavityTime2;
-		hid_t phaseCavityCharge1;
-		hid_t phaseCavityCharge2;
-		hid_t photon_energy_eV;
-		hid_t photon_wavelength_A;
-		hid_t f_11_ENRC;
-		hid_t f_12_ENRC;
-		hid_t f_21_ENRC;
-		hid_t f_22_ENRC;
-		hid_t evr41;
-		std::vector<hid_t> detector_positions;
-		std::vector<hid_t> detector_EncoderValues;
-		hid_t eventTimeString;
-		hid_t tofTime;
-		hid_t tofVoltage;
-	}LCLS;
-
-	typedef struct{
-		hid_t self;
-	}ConfValues;
-
-	typedef struct{
-		hid_t self;
-		hid_t eventName;
-		hid_t frameNumber;
-		hid_t frameNumberIncludingSkipped;
-		hid_t threadID;
-		hid_t gmd1;
-		hid_t gmd2;
-		hid_t energySpectrumExist;
-		hid_t nPeaks;
-		hid_t peakNpix;
-		hid_t peakTotal;
-		hid_t peakResolution;
-		hid_t peakResolutionA;
-		hid_t peakDensity;
-		hid_t laserEventCodeOn;
-		hid_t laserDelay;
-		hid_t hit;
-		std::vector<hid_t> sums;
-	}UnsharedValues;
-
-	typedef struct{
-		hid_t self;
-		hid_t lastBgUpdate[MAX_DETECTORS];
-		hid_t nHot[MAX_DETECTORS];
-		hid_t lastHotPixUpdate[MAX_DETECTORS];
-		hid_t hotPixCounter[MAX_DETECTORS];
-		hid_t nHalo[MAX_DETECTORS];
-		hid_t lastHaloPixUpdate[MAX_DETECTORS];
-		hid_t haloPixCounter[MAX_DETECTORS];
-		hid_t hit;
-		hid_t nPeaks;
-	}SharedValues;
-
-	typedef struct{
-		hid_t self;
-		//needs to be implemented:
-		//hid_t cheetah_version;
-		ConfValues confVal;
-		UnsharedValues unsharedVal;
-		SharedValues sharedVal;
-	}CheetahValues;
-
-	typedef struct{
-		std::vector<hid_t> radialAverage;
-		std::vector<hid_t> radialAverageCounter;
-	}Radial;
-
-	typedef struct{
-		hid_t self;
-	}Data;
-
-	typedef struct{
-		hid_t self;
-
-		Data data;
-		Instrument instrument;
-		hid_t experimentIdentifier;
-		std::vector<Image> images;
-		Sample sample;
-	}Entry;
-
-	typedef struct{
-		hid_t self;
-		Entry entry;
-		LCLS lcls;
-		CheetahValues cheetahVal;
-  
 		/*  This counter defines where in the file each image is stored.
 		 *  It is atomically incremented by each thread */
 		uint stackCounter;
-		Node * root;
-	}File;
 
+	};
 
 	const int version = 130;
 	const int thumbnailScale = 8;
