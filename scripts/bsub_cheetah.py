13e59c46bbe7f06e0cd3caaa77779457584a9869
Max Hantke <hantke@xray.bmc.uu.se>
2014-07-23 23:44:52 -0700

    working version of submission script

--- scripts/bsub_cheetah.py
+++ scripts/bsub_cheetah.py
@@ -13,12 +13,12 @@ password = getpass.getpass()
 
 # Find this value in the url with 'key=XXX' and copy XXX below
 spreadsheet_key = '1hAWODbWaFRUnmIoTO1LhxS8tEcyrwzJu_sN2jlkEmc0'
-#spreadsheet_key = '1YUYX53zULUBthrczoEOQ6mClTLNO43H3Xl_A7yvym74'
-# All spreadsheets have worksheets. I think worksheet #1 by default always
-# has a value of 'od6'
-#worksheet_id = '329921770'
 worksheet_id = 'obafa2y'
-#worksheet_id = 'od6'
+
+run_col = 0
+run_type_col = 1
+
+dry_run = False
 
 spr_client = gdata.spreadsheet.service.SpreadsheetsService()
 spr_client.email = email
@@ -30,37 +30,68 @@ def get_xtcs(run):
     L = os.listdir(xtcdir)
     M = [run in l for l in L]
     return L[M]
-def get_processed_run_set():
-    return set(os.listdir(h5dir))
+def get_submitted_run_set():
+    L_data = os.listdir(h5dir)
+    lens = np.array([len(l) for l in L_data],dtype="int")
+    L_data = np.array(L_data)[lens>5]
+    L_data = [l[:5] for l in L_data]
+    L = set(L_data)
+    L_dark = os.listdir(h5dir_dark)
+    lens = np.array([len(l) for l in L_dark],dtype="int")
+    L_dark = np.array(L_dark)[lens>5]
+    L_dark = [l[:5] for l in L_dark]
+    L = L.union(set(np.array(L_dark)))
+    return L
 def get_available_run_set():
     xtcs = os.listdir(xtcdir)
-    runs = set([xtc[5:5+5] for xtc in xtcs])
+    #print xtcs
+    runs = set([xtc[5:5+5] for xtc in xtcs]) - set([""])
     return runs
-def get_folders_run_set():
-    L = os.listdir(h5dir)
-    M = [(len(l) == 5) for l in L]
-    LS = []
-    for m,l in zip(M,L):
-        if m:
-            LS.append(l)
-    return set(LS)
+def get_folders_run_list():
+    L = []
+    R = []
+    L_data = os.listdir(h5dir)
+    lens = np.array([len(l) for l in L_data],dtype="int")
+    L_data = list(np.array(L_data)[lens>5])
+    for l in L_data:
+	L.append(h5dir+"/"+l)
+	R.append(l[:5])
+    L_dark = os.listdir(h5dir_dark)
+    lens = np.array([len(l) for l in L_dark],dtype="int")
+    L_dark = list(np.array(L_dark)[lens>5])
+    for l in L_dark:
+	L.append(h5dir_dark+"/"+l)
+	R.append(l[:5])
+    L.sort()
+    R.sort()
+    return R,L
 def prepare_run(run):
+    print "prepare",run
+    run_type = runs_types[run]
     t = time.time()
     st = datetime.datetime.fromtimestamp(t).strftime('%Y%m%d_%H%M%S')
-    runfolder = h5dir + "/" + run + "_" + st + "/"
-    runlink = h5dir + "/" + run
+    if run_type == "data": 
+        darkcal = get_prior_darkcal(run)
+        print "dark",darkcal
+        if darkcal == None:
+            return None,None
+	runfolder = h5dir + "/" + run + "_" + st
+	runlink = h5dir + "/" + run
+    elif run_type == "dark":
+	runfolder = h5dir_dark + "/" + run + "_" + st
+	runlink = h5dir_dark + "/" + run
     os.system("mkdir %s" % runfolder)
     runfile = runfolder + "/process.sh"
     outfile = runfolder + "/bsub.out"
-    cheetah_configfile = "%s/cheetah_%s.ini" % (confdir,run)  
-    if not os.path.ispath(cheetah_configfile):
-        cheetah_configfile = "%s/cheetah.ini" % (confdir)  
-    os.system("cp %s %s" % (cheetah_configfile,runfolder))
+    cheetah_configfile = get_cheetah_configfile(run,run_type)
+    os.system("cp %s %s/cheetah.ini" % (cheetah_configfile,runfolder))
+    if run_type == "data":
+	insert_darkcal_in_cheetah_configfile(darkcal,"%s/cheetah.ini" % runfolder)
     psana_configfile = "%s/psana_%s.cfg" % (confdir,run)  
-    if not os.path.ispath(psana_configfile):
+    if not os.path.isfile(psana_configfile):
         psana_configfile = "%s/psana.cfg" % (confdir)  
     psana_configfile = confdir + "/psana.cfg"
-    os.system("cp %s %s" % (psana_configfile,runfolder))
+    os.system("cp %s %s/psana.cfg" % (psana_configfile,runfolder))
     txt = ["#!/bin/bash\n"]
     txt += ["source %s\n" % environment]
     txt += ["cd %s\n" % runfolder]
@@ -70,6 +101,68 @@ def prepare_run(run):
         f.writelines(txt)
     os.system("chmod u+x %s" % runfile)
     return outfile,runfile
+def get_runs_types():
+    #print "getrt"
+    g_runs = read_col_google(run_col)[1:]
+    g_types = read_col_google(run_type_col)[1:]
+    #print g_runs,g_types
+    D = {}
+    N = min([len(g_runs),len(g_types)])
+    for r,t in zip(g_runs[:N],g_types[:N]):
+	D[r] = t
+    return D
+def get_cheetah_configfile(run,run_type):
+    if run_type == "data":
+	cheetah_configfile = "%s/cheetah_template_%s.ini" % (confdir,run)
+        if not os.path.isfile(cheetah_configfile):
+	    cheetah_configfile = "%s/cheetah_template.ini" % (confdir)
+    elif run_type == "dark":
+	cheetah_configfile = "%s/cheetah_%s.ini" % (confdir_dark,run)  
+        if not os.path.isfile(cheetah_configfile):
+	    cheetah_configfile = "%s/cheetah.ini" % (confdir_dark)
+    return cheetah_configfile
+def insert_darkcal_in_cheetah_configfile(darkcal,cheetah_configfile):
+    with open(cheetah_configfile,"r") as f:
+	ls = f.readlines()
+	ls_n = []
+	for l in ls:
+	    if l[0] == "#":
+		ls_n.append(l)
+	    else:
+		if "darkcal" in l:
+		    ls_n.append("darkcal=%s\n" % darkcal)
+		else:
+                    if "=" in l:
+                        ls_n.append(os.path.expandvars(l.split("=")[0]) + "=" + os.path.expandvars(l.split("=")[1]))	    
+                    else:
+                        ls_n.append(l)
+    #print ls_n
+    #sys.exit()
+    with open(cheetah_configfile,"w") as f:
+	f.writelines(ls_n)
+def get_prior_darkcal(run):
+    run_nr = int(run[1:])
+    dirs = os.listdir(h5dir_dark)
+    lens = [len(l) for l in dirs]
+    L = []
+    for i,l in enumerate(lens):
+	if l == 5:
+	    L.append(dirs[i][1:])
+    if L == []:
+	return None
+    L = np.array(L,dtype="int")
+    if L.min() > run_nr:
+	return None
+    #print L,run_nr
+    L = L[run_nr > L]
+    #print L
+    drun = "r%04i" % L[-1]
+    # check whether run is finished
+    print runs_info
+    if runs_info.get(drun,{}).get("status",None) != "done":
+        return None
+    darkcal = h5dir_dark + "/" + drun + "/" + ("%s-pnCCD-darkcal.h5" % drun)
+    return darkcal
 def get_terminal_shape():
     rows, columns = os.popen('stty size', 'r').read().split()
     return (int(rows),int(columns))  
@@ -78,61 +171,97 @@ def clear_screen():
     sys.stdout.write("\033[H")
     sys.stdout.flush()
 def update_runs_info():
-    global finished_runs
     global changed
     global runs_info
-    folders_runs = get_folders_run_set()
-    folders_runs = list(folders_runs - finished_runs)
-    for l in folders_runs:
-        logf = h5dir+"/"+l+"/log.txt"
+    global finished_runs
+    global submitted_runs
+    runs_info = {}
+    runs,folders_runs = get_folders_run_list()
+    #print runs,folders_runs
+    for r,l in zip(runs,folders_runs):
+	runs_info[r]={}
+        runs_info[r]["type"] = runs_types[r]
+        logf = l+"/log.txt"
         if os.path.isfile(logf):
-            runs_info[l] = {} 
             with open(logf,"r") as f:
                 loglines = f.readlines()
                 if ">-------- End of job --------<\n" in loglines:
-                    runs_info[l]["status"] = "done"
+                    runs_info[r]["status"] = "done"
                     for line in loglines:
                         if "Frames processed:" in line:
-                            runs_info[l]["#frames"] = line.split(" ")[-1][:-1]
+                            runs_info[r]["#frames"] = line.split(" ")[-1][:-1]
                         elif "Number of hits:" in line:
-                            runs_info[l]["#hits"] = line.split(" ")[-1][:-1]
+                            runs_info[r]["#hits"] = line.split(" ")[-1][:-1]
                         elif "Average hit rate:" in line:
-                            runs_info[l]["hit rate"] = line.split(" ")[-2][:-1]
-                    finished_runs.add(l)
-                    changed = True
+                            runs_info[r]["hrate"] = line.split(" ")[-2][:-1]
+		    finished_runs.add(r)
                 else:
-                    runs_info[l]["status"] = "submitted"
+                    runs_info[r]["status"] = "subm"
+		    submitted_runs.add(r)
+	    changed = True
+	else:
+	    runs_info[r]["status"] = "-"
 def print_table_terminal():
-    rs = runs_info.keys()
+    #rs = runs_info.keys()
+    rs = runs_types.keys()
     rs.sort()
     print "# Cheetah queue status"
     print ("-"*term_shape[1])
-    print ("Run\tstatus\t#frames\t#hits\thit rate")
+    print ("Run\ttype\tstatus\t#frames\t#hits\thrate")
     for i in range(term_shape[0]-5):
         if i <= (len(rs)-1):
-            d = runs_info[rs[i]]
-            print rs[i] + "\t" + d["status"] + "\t" + d["#frames"] + "\t" + d["#hits"] + "\t" + d["hit rate"]
+            d = runs_info.get(rs[i],{})
+	    #print runs_info,rs[i]
+	    print rs[i] + "\t" + runs_types[rs[i]] + "\t" + d.get("status","-") + "\t" + d.get("#frames","-") + "\t" + d.get("#hits","-") + "\t" + d.get("hrate","-")
         else:
-            print "-\t-\t-\t-\t-" 
+            print "-\t-\t-\t-\t-\t-" 
     print ("-"*term_shape[1])
 def write_table_google():
-    write_row_google(0,["Run","status","#frames","#hits","hit rate [%]"])
+    write_row_google(0,["Run","Type","Status","#Frames","#Hits","Hit rate [%]"])
     rs = runs_info.keys()
     rs.sort()
     for i,r in zip(range(len(rs)),rs):
         d = runs_info[rs[i]]
-        write_row_google(i+1,[rs[i],d["status"],d["#frames"],d["#hits"],d["hit rate"]])
+        write_row_google(i+1,[rs[i],d["type"],d["status"],d.get("#frames",""),d.get("#hits",""),d.get("hrate","")])
 def write_row_google(row,values): 
     for col,value in zip(range(len(values)),values):
         entry = spr_client.UpdateCell(row=row+1, col=col+1, inputValue=value,
                                       key=spreadsheet_key, wksht_id=worksheet_id)
-
-
+def read_row_google(row):
+    cells = []
+    col = 0
+    while True:
+	c = read_cell_google(row,col)
+	if c != None:
+	    cells.append(c)
+	    col += 1
+	else:
+	    break
+    return cells
+def read_col_google(col):
+    cells = []
+    row = 0
+    while True:
+	c = read_cell_google(row,col)
+	if c != None:
+	    cells.append(c)
+	    row += 1
+	else:
+	    break
+    return cells
+def read_cell_google(row,col):
+    s = spr_client.GetCellsFeed(key=spreadsheet_key, wksht_id=worksheet_id,cell="R%iC%i" % (row+1,col+1)).content.text
+    #print s
+    return s
+def get_runs_with_valid_types_set():
+    ir = set([])
+    for r,t in runs_types.items():
+	if t in ["data","dark"]:
+	    ir.add(r)
+    return ir
 
 if len(sys.argv) < 2:
     sys.exit("Usage: busb_cheetah.py configurationfile")
-
-
 
 hostname = socket.gethostname()
 if not ("psexport" in hostname or "pslogin" in hostname):
@@ -142,32 +271,52 @@ configfilename = sys.argv[1]
 
 C = configobj.ConfigObj(configfilename)
 h5dir = os.path.expandvars(C["h5dir"])
+h5dir_dark = os.path.expandvars(C["h5dir_dark"])
 xtcdir = os.path.expandvars(C["xtcdir"])
 confdir = os.path.expandvars(C["confdir"])
+confdir_dark = os.path.expandvars(C["confdir_dark"])
 environment = os.path.expandvars(C["environment"])
 
-processed_runs = get_processed_run_set()
-submitted_runs = set([])
+processed_runs = set([])
+submitted_runs = get_submitted_run_set()
 finished_runs = set([])
 runs_info = {}
 info_submitted_runs = {}
 
 while True:
-    sys.stdout.write("\r- Checking process status...                        ")
-    sys.stdout.flush()
     changed = False
     term_shape = get_terminal_shape()
+    sys.stdout.write("\r- Checking process status...                        ")
+    sys.stdout.flush()
+    runs_types = get_runs_types()
+    runs_with_types_set = get_runs_with_valid_types_set()
+    available_runs = get_available_run_set()
+    print "ar",available_runs
+    print "sr",submitted_runs
+    print "rwt",runs_with_types_set
+    update_runs_info()
+    runs_to_process = (available_runs - submitted_runs).intersection(runs_with_types_set)
     clear_screen()
     print_table_terminal()
-    available_runs = get_available_run_set()
-    runs_to_process = available_runs - processed_runs - submitted_runs
+    print runs_to_process
     if len(runs_to_process) > 0:
-        for run in runs_to_process:
-            print "Submitting job for run %s..." % run
-            outfile,runfile = prepare_run(run)
-            os.system("bsub -q psnehq -o %s %s" % (outfile,runfile))
-            submitted_runs.add(run)
-            changed = True
+	tmp = list(runs_to_process)
+	tmp.sort()
+        run = tmp[0]
+	sys.stdout.write("Submitting job for run %s..." % run)
+	sys.stdout.flush()
+	outfile,runfile = prepare_run(run)
+	if not dry_run and outfile != None and runfile != None:
+	    substr = "bsub -q psnehq -o %s %s" % (outfile,runfile)
+	    os.system(substr)
+	    sys.stdout.write("\r"+substr)
+	    sys.stdout.flush()
+	    submitted_runs.add(run)
+	    changed = True
+	else:
+	    sys.stdout.write("\r"+"Submission postponed...                       ")
+	    sys.stdout.flush()
+	    time.sleep(1.)
     else:
         dt = 10
         mill = ["-","\\","|","/"]
@@ -176,7 +325,6 @@ while True:
             sys.stdout.write("\r"+mill[i%len(mill)] + " Waiting for new XTC files to appear...")
             sys.stdout.flush()
             time.sleep(0.1)
-    update_runs_info()
     if changed:
         sys.stdout.write("\r- Writing to google spreadsheet...                        ")
         sys.stdout.flush()
