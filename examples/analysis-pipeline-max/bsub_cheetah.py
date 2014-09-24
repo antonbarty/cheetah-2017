20281d4c4a7c379298896914bc54665e3bbb2906
Max Hantke <hantke@xray.bmc.uu.se>
2014-09-24 06:33:30 -0700

    bsub_cheetah tested the first time after beam time.

--- examples/analysis-pipeline-max/bsub_cheetah.py
+++ examples/analysis-pipeline-max/bsub_cheetah.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+import configobj
 import os,sys
+import time,datetime
 import socket
-import runner
+import numpy as np
+import run,terminal,google
+import getpass
 
-fqdn = socket.getfqdn()
+# Are we on the right host machine?
+hostname = socket.gethostname()
+if not ("psexport" in hostname or "pslogin" in hostname):
+    sys.exit("ERROR: Cannot submit jobs from this host. You are logged in to %s but you need to be logged in to psexport of pslogin to submit jobs to the queue." % hostname)
 
-# Are we on a suitable host machine?
-if not ((("psexport" in fqdn or "pslogin" in fqdn) and "slac" in fqdn) or ("login" in fqdn and "davinci" in fqdn)):
-    sys.exit("ERROR: Cannot submit jobs from this host. You are logged in to %s but you need to be logged in to pslogin.slac.stanford.edu, psexport.slac.stanford.edu or davinci.icm.uu.se to submit jobs to a queue." % (fqdn))
-
-# Check configuration
+# Read configuration
 if len(sys.argv) < 2:
-    sys.exit("Usage: busb_cheetah.py bsub_cheetah.cfg [DEBUG]")
+    sys.exit("Usage: busb_cheetah.py bsub_cheetah.cfg")
 
-# Are we running in debugging mode?
 debug = False
+#print sys.argv
+#sys.exit()
 if len(sys.argv) >= 3:
     if sys.argv[2] == "DEBUG":
         debug = True
@@ -93,10 +97,10 @@ def main():
     #    time.sleep(5)
 
 if debug:
-    runner.run(configfilename)
+    main()
 else:
     while True:
         try:
-            runner.run(configfilename)
+            main()
         except:
             print "WARNING: run() crashed, restarting..."
