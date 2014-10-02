176985279eaa0fd367b627204944ce403120d3c2
hantke@xray.bmc.uu.se <hantke@xray.bmc.uu.se>
2014-10-02 12:26:57 +0200

    added cheetah_ana_mod to rpath

--- examples/analysis-pipeline-max/terminal.py
+++ examples/analysis-pipeline-max/terminal.py
@@ -2,7 +2,9 @@ import os,sys
 import subprocess
 
 class TerminalTable:
-    def __init__(self):
+    def __init__(self,conf):
+        self.conf = conf
+        self.mill_index = 0
         self.refresh_terminal_shape()
         self.col_names = ["Run","Type","Status","#Frames","#Hits","HRate"]
         self.title = ["# Cheetah queue status",("-"*self.shape[1]),"",("-"*self.shape[1])]
@@ -15,7 +17,6 @@ class TerminalTable:
         self.print_screen()
     def set_runs(self,runs):
         self.runs = runs
-        self.message = ["Updating run information"]
         self.print_screen()
     def _get_s_head(self):
         s_head = ""
@@ -24,9 +25,12 @@ class TerminalTable:
         l = self.shape[0]-len(s)-4
         s_head += "||"+" "*(l/2)+s+" "*(l/2)+"||\n"
         s_head += "="*self.shape[0]+"\n"
-        for c in self.col_names:
-            s_head += c + "\t"
-        s_head = s_head[:-1] + "\n"
+        width = self.shape[0]/56
+        for w in range(width):
+            for c in self.col_names:
+                s_head += c + "\t"
+            s_head += "   ||   "
+        s_head = s_head[:-9] + "\n"
         return s_head
     def _get_s_message(self):
         s_message = ""
@@ -38,21 +42,31 @@ class TerminalTable:
         names = self.runs.keys()
         names.sort()
         length = self.shape[1]-8-self.Nlines_jobs-(self.Nlines_jobs>0)
-        print length
-        print len(names)
-        for i in range(length):
+        width = self.shape[0]/56
+        #print length
+        #print len(names)
+        s_content_lines = [""] * length
+        for i in range(length*width):
             if i <= (len(names)-1):
                 R = self.runs[names[-i-1]]
-                s_content += names[-i-1] + "\t"
+                s_content_lines[i%length] += names[-i-1] + "\t"
                 for c in self.col_names[1:]:
-                    s_content += R.attrs.get(c,"-") + "\t"
-                s_content = s_content[:-1] + "\n"
+                    s_content_lines[i%length] += R.attrs.get(c,"-") + "\t"
+                s_content_lines[i%length] += "   ||   "
             else:
-                s_content += ("-\t"*len(self.col_names))[:-1] + "\n"
+                s_content_lines[i%length] += ("-\t"*len(self.col_names)) + "   ||   "
+        s_content = ""
+        for i in range(length):
+            s_content += s_content_lines[i][:-9] + "\n"
         return s_content
     def _get_s_jobs(self):
         s_jobs = ""
-        p = subprocess.Popen(["bjobs"],stdout=subprocess.PIPE)
+        if os.path.expandvars(self.conf["general"]["job_manager"]) == "lsf":
+            c = ["bjobs"]
+        elif os.path.expandvars(self.conf["general"]["job_manager"]) == "slurm":
+            c = ["squeue",os.path.expandvars("-u$USER")]
+        #print c
+        p = subprocess.Popen(c,stdout=subprocess.PIPE)
         p.wait()
         lines = p.stdout.readlines()
         self.Nlines_jobs = len(lines)
@@ -80,7 +94,9 @@ class TerminalTable:
         if self.Nlines_jobs > 0:
             sys.stdout.write(s_jobs)
             sys.stdout.write("-"*self.shape[0]+"\n")
-        sys.stdout.write(s_message)
+        mill =  ["-","\\","|","/"][self.mill_index%4]
+        self.mill_index += 1
+        sys.stdout.write(mill + " " +  s_message)
         sys.stdout.flush()
         
 
