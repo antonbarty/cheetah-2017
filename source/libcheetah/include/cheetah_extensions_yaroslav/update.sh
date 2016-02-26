e5260066034402052a1049a1e669158391b46411
Valerio Mariani <valerio.mariani@desy.de>
2016-02-26 14:27:30 +0100

    Removed update scripts that are not needed in cheetah

--- source/libcheetah/include/cheetah_extensions_yaroslav/update.sh
+++
@@ -1,3 +0,0 @@
-mv streakfinder.h peakfinder9.h ../
-for f in `ls *.h`; do cp ../../cheetah_extensions_yaroslav/include/$f ./; done
-mv ../streakfinder.h ../peakfinder9.h ./
--- source/libcheetah/src/cheetah_extensions_yaroslav/update.sh
+++
@@ -1,3 +0,0 @@
-mv streakfinder.cpp peakfinder9.cpp ../
-for f in `ls *.cpp`; do cp ../../cheetah_extensions_yaroslav/src/$f ./; done
-mv ../streakfinder.cpp ../peakfinder9.cpp ./
